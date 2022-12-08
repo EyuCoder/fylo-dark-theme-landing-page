6abe00b44cbec08fbe5cfaed7f6226522a4219f0
venom <eyueldaniel0921@gmail.com>
2022-12-08 11:02:30 +0300

    Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,8 @@ function App() {
   return (
     <div className='dark:bg-darkBlue dark:text-white font-opensans'>
       <Header />
+      <Hero />
+
     </div>
   )
 }
